69322aa9567f7f40b07f19bf6e502c1bc92978dd
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2024-02-20 11:46:22 -0800

    Adding the user login feature

--- routes/index.js
+++ routes/index.js
@@ -52,6 +52,8 @@ router.get("/register", function (req, res, next) {
   return res.render("register", { title: "Register" });
 });
 
+
+
 // register
 router.post("/register", function (req, res, next) {
   if (req.body.name && req.body.favoriteBook && req.body.password && req.body.email && req.body.confirmPassword) {
@@ -84,6 +86,23 @@ router.post("/register", function (req, res, next) {
     err.status = 400;
     return next(err);
   }
+});
+
+// GET /profile
+router.get('/profile', function(req, res, next) {
+  if (! req.session.userId ) {
+    var err = new Error("You are not authorized to view this page.");
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId)
+      .exec(function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook });
+        }
+      });
 });
 
 module.exports = router;
