3071796b49fd9eda753a431e4c90ffe0ab1499e0
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2024-02-20 12:11:43 -0800

    adding logout button

--- routes/index.js
+++ routes/index.js
@@ -105,4 +105,17 @@ router.get('/profile', function(req, res, next) {
       });
 });
 
+// GET /logout
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
 module.exports = router;
